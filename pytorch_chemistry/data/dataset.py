de49baa0531f00561b43c4fdee018590f4a67324
0h-n0 <kbu94982@gmail.com>
2018-06-05 16:17:46 +0900

    feat:

---
+++ pytorch_chemistry/data/dataset.py
@@ -0,0 +1,15 @@
+
+
+class Dataset(object):
+    def __init__(self, parser):
+        self.parser = parser
+        pass
+
+    def __getitem__(self):
+        pass
+
+
+    def __len__(self):
+        pass
+    
+
