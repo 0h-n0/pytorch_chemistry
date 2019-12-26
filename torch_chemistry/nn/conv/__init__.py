4ba17e6f5daa259397a7b8a7c37550f173fc18d2
0h-n0 <kbu94982@gmail.com>
2019-12-26 10:00:06 +0900

    feat: add feedforward_init.

--- torch_chemistry/nn/conv/__init__.py
+++ torch_chemistry/nn/conv/__init__.py
@@ -1,6 +1,22 @@
+import torch
 import torch.nn as nn
 
 
 class GNNConv(nn.Module):
     def __init__(self):
         super(GNNConv, self).__init__()
+
+    def _laplacianize(self, adj):
+        return self._normalize(self._add_identity(adj))
+
+    def _add_identity(self, adj):
+        B, N, _ = adj.size()
+        eyes = torch.eye(N, dtype=adj.dtype, device=adj.device).reshape(-1, N, N).repeat(B, 1, 1)
+        return adj.add(eyes)
+
+    def _normalize(self, adj):
+        inv_sqrt_degree_mat = 1 / torch.sqrt(torch.diag_embed(adj.sum(axis=2)))
+        inv_sqrt_degree_mat[inv_sqrt_degree_mat == float('inf')] = 0
+        adj = adj.matmul(inv_sqrt_degree_mat)
+        normalized_adj = inv_sqrt_degree_mat.matmul(adj)
+        return normalized_adj
